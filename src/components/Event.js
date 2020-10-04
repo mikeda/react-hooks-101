abb00a5f31e25eed014f4337ebcc7c16441fb3be
mikeda <baguzy@gmail.com>
2020-10-04 22:57:57 +0900

    削除系のイベント調整

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,10 +3,9 @@ import React from 'react';
 const Event = ({ event, dispatch }) => {
   const id = event.id;
   const handleClickDeleteButton = () => {
-    dispatch({
-      type: 'DELETE_EVENT',
-      id
-    })
+    const result = window.confirm(`${id}を削除していいですか？`);
+
+    if(result) dispatch({ type: 'DELETE_EVENT', id })
   }
 
   return (<tr>
