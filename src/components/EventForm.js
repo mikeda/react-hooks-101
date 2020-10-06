e6678b477fd4a32845c7d735055b9bbb39426c5f
mikeda <baguzy@gmail.com>
2020-10-07 08:21:05 +0900

    操作ログの削除ボタン

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -47,7 +47,18 @@ const EventForm = () => {
     }
   }
 
+  const deleteAllOperationLogs = e => {
+    e.preventDefault();
+
+    const result = window.confirm('全ての操作ログを削除していいですか？');
+
+    if (result) {
+      dispatch({ type: DELETE_ALL_OPERATION_LOGS });
+    }
+  }
+
   const unCreatable = title === '' || body === '';
+
 
   return(
     <>
@@ -65,6 +76,7 @@ const EventForm = () => {
 
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
         <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllOperationLogs} disabled={state.operationLogs.length === 0}>全ての操作ログを削除する</button>
       </form>
     </>
   )
